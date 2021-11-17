1b943ece4bc4b10a386b18166b9e0ba22bd60c8b
Ryan Anderson <sonorousduck@gmail.com>
2021-11-17 08:02:04 -0700

    Working on getting the requests working

--- smashBot/views.py
+++ smashBot/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
+from django.http import JsonResponse, FileResponse
+import json
+import numpy as np
+import io
+from numpy import unicode
 
 # Create your views here.
+
+
+#class NumpyAwareJSONEncoder(json.JSONEncoder):
+#    """
+#        This class facilitates the JSON encoding of Numpy onjects.
+#        e.g. numpy arrays are not supported by the standard json encoder - dumps.
+#        """
+#        def default(self, obj):
+#            if isinstance(obj, np.ndarray):
+#                return obj.tolist()
+#            return json.JSONEncoder.default(self, obj)
+#
 
 
 def index(request):
@@ -10,17 +27,26 @@ def index(request):
 async def postState(request):
     # This is where the training logic is going to go
 
+
+        
+
     pass
 
 
 async def getAgent(request):
 
     # get the agent from the file
+   
+    model = open('smashBot/recentWeights.hdf5', 'rb')
+
+    response = FileResponse(model)
+
+    response['Access-Control-Allow-Origin'] = '*'
+
+    return response
     
-
-
-
-    pass
+    #model = {}
+    #response = JsonResponse({'model': model})
 
 
 
